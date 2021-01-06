fa65799c58fa6de06b0a6c1d5a5611194d056bec
Rommel Torquator <rommeltorquator09@gmail.com>
2021-01-06 21:30:30 +0800

    Added the quiz activity

---
+++ quiz/app.js
@@ -0,0 +1,74 @@
+function Question(ans) {
+    // questions
+    let questions = ['What is the color of Orange?', 'How old is Jose Rizal when he died?', 'How many sides does the triange have?'];
+    
+    // generates the random question
+    let rand = Math.floor(Math.random() * questions.length + 1);
+    
+    // choices
+    let c1 = ['Orange', 'Green', 'Blue']; // 1
+    let c2 = [41, 35, 29]; // 2
+    let c3 = [3, 2, 1]; // 1
+    let n = 0;
+    
+    // displaying the questions
+    function showQuestion() {
+        console.log(questions[rand - 1]);
+    }
+    
+    // displaying the choices
+    function showChoices(num) {
+        switch(num - 1) {
+            case 0:
+                c1.forEach((name) => console.log(`${++n}) ${name}`));
+                break;
+            case 1:
+                c2.forEach((name) => console.log(`${++n}) ${name}`));
+                break;
+            case 2:
+                c3.forEach((name) => console.log(`${++n}) ${name}`));
+                break;
+        }
+    }
+    
+    // asking the user for the answer
+    function askAnswer(r) {
+        // stores the answer
+        let ans = prompt("Answer: ");
+        ans = parseInt(ans);
+        
+        // selects the question
+        switch(r - 1) {
+            case 0:
+                // evaluates the answer for question 1
+                if(ans === 1) {
+                    console.log("Correct");
+                } else {
+                    console.log("Wrong!");
+                }
+                break;
+            case 1:
+                // evaluates the answer for question 1
+                if(ans === 2) {
+                    console.log("Correct");
+                } else {
+                    console.log("Wrong!");
+                }
+                break;
+            case 2:
+                // evaluates the answer for question 1
+                if(ans === 1) {
+                    console.log("Correct");
+                } else {
+                    console.log("Wrong!");
+                }
+                break;
+        }
+    }
+    
+    showQuestion();
+    showChoices(rand);
+    askAnswer(rand);
+}
+
+Question();
