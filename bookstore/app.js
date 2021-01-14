44854647327751744d56a787402abf1050403971
Rommel Rufila Torquator <rommel.torquator@infinit-o.com>
2021-01-13 21:13:44 -0500

    Finished the bookstore activity

--- bookstore/app.js
+++ bookstore/app.js
@@ -15,6 +15,11 @@ let store = {
             title: "Book of the Dead",
             quantity: 3,
             price: 100
+        },
+        {
+            title: "Eleven Rings: The Soul of Success",
+            quantity: 50,
+            price: 300
         }
     ],
     earnings: 0
@@ -40,25 +45,46 @@ const sellBook = (title, quantity) => {
     let notAvailable = true;
     store.inventory.forEach(i => {
         // checks if the books is available and has enough quantity
-        if(i.title === title && i.quantity > quantity) {
+        if(i.title === title && i.quantity >= quantity) {
             // decreases the quantity
             let subtractQuantity = i.quantity - quantity;
             i.quantity = subtractQuantity;
 
             // adds the price to the total earnings
-            store.earnings += i.price * quantity;         
+            store.earnings += i.price * quantity;      
             notAvailable = false;
             console.log(`Successful transaction`);
+        } else if(i.title === title) {
+            notAvailable = false;
+            checkQuantity(title, quantity, notAvailable);
         }
     });
 
-    checkAvailability(notAvailable, title);    
+    checkAvailability(notAvailable, title);
 };
 
 // check if the book exists in the inventory
-const checkAvailability = (b, t) => {
-    if(b) {
-        console.log(`${t} is out of stock`);
+const checkAvailability = (available, title) => {
+    if(available) {
+        console.log(`${title} is out of stock`);
+    }
+}
+
+// check the remaining quantity
+const checkQuantity = (title, quantity, b) => {
+    let book = {};
+    let showRemaining = false;
+
+    store.inventory.forEach(i => {
+        // checks if the books is available
+        if(i.title === title && i.quantity < quantity && b === false) {
+            book = i;
+            showRemaining = true;
+        }
+    });
+
+    if(showRemaining) {
+        console.log(`only ${book.quantity} stocks left`);
     }
 }
 
@@ -68,8 +94,8 @@ const totalEarnings = () => console.log(store.earnings);
 // shows the list of books including the price and quantity
 const listInventory = () => store.inventory.forEach(i => console.log(`Title: ${i.title}, Price: ${i.price}, Quantity: ${i.quantity}`));
 
-// addBook("Eleven Rings: The Soul of Success", 50, 300);
-// restockBook("Book of the Dead", 50);
-sellBook("Harry Potter and the Deathly Hallows", 1);
+addBook("The Bible", 20, 50);
+restockBook("Book of the Dead", 5000);
+sellBook("Book of the Dead", 4);
 totalEarnings();
 listInventory();
