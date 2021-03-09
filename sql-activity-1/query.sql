1aeee3c72916fb31408da175a3f87a7fc64533d4
Rommel Torquator <rommeltorquator09@gmail.com>
2021-03-09 07:12:36 -0500

    Added the March 9, 2021 activity

--- sql-activity-1/query.sql
+++ sql-activity-1/query.sql
@@ -1,3 +1,5 @@
+***** 3-8-2021 activity *****
+
 CREATE TABLE students(
     id INT,
     first_name VARCHAR(50),
@@ -8,15 +10,28 @@ CREATE TABLE students(
 );
 
 INSERT INTO students (id, first_name, middle_name, last_name, age, location)
-VALUES (1, "Rommel", "Rufila", "Torquator", 30, "Makati"),
-(2, "Kevin", "Kosme", "Kosmos", 65, "Bulacan"),
-(3, "Anita", "Leonor", "Perez", 72, "Boracay"),
-(4, "Anna", "Anita", "Cruz", 22, "Pasay"),
-(5, "Melo", "Blank", "Anthony", 33, "New York"),
-(6, "Tarzan", "Blank", "Jones", 44, "Iraq");
+VALUES (1, "Juan", "Blank", "Cruz", 18, "Manila"),
+(2, "John", "Blank", "Young", 20, "Manila"),
+(3, "Victor", "Blank", "Rivera", 21, "Manila"),
+(4, "Adrian", "Blank", "Co", 19, "Laguna"),
+(5, "Pau", "Blank", "Riosa", 22, "Marikina"),
+(6, "Piolo", "Blank", "Pascual", 25, "Manila");
 
 UPDATE students
 SET first_name = "Ana", middle_name = "Cui", last_name = "Cajocson", age = 25, location = "Bulacan"
 WHERE id = 1;
 
 DELETE FROM students WHERE id = 6;
+
+***** 3-9-2021 activity *****
+
+SELECT COUNT(*) FROM STUDENTS;
+
+SELECT * FROM STUDENTS 
+WHERE location = "Manila";
+
+SELECT AVG(age) FROM STUDENTS;
+
+SELECT * FROM STUDENTS
+ORDER BY age DESC;
+
